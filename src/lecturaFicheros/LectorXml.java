d9aeaa2c442f0e4400a202a2337227a9e7c1e957
IN1DM3B_22 <IN1DM3B_22@H015112.NET>
2019-10-21 14:11:32 +0200

    ficheros

--- src/lecturaFicheros/LectorXml.java
+++ src/lecturaFicheros/LectorXml.java
@@ -14,7 +14,8 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class LectorXml {
 
-	public void leerXml(String path) {
+	public static void main(String args[]) {
+	//public void leerXml(String path) {
 		try {
     		
             SAXParserFactory parserFactory = SAXParserFactory.newInstance();
@@ -22,60 +23,32 @@ public class LectorXml {
 
             DefaultHandler handler = new DefaultHandler() {
 
-            	boolean bTitulo = false;
-                boolean bAutor = false;
-                boolean bGenero = false;
-                boolean bDescripcion = false;
+                boolean bDepartamento = false;
+                int id = 0;
+                String departamento = null;
 
                 public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
                 	
-            		if (qName.equalsIgnoreCase("libro")) {
-            			String id = attributes.getValue("id");
-            			System.out.println("id: " + id);
+            		if (qName.equalsIgnoreCase("departamento")) {
+            			id = Integer.parseInt(attributes.getValue("id"));
+            			bDepartamento = true;
             		}
-            		
-            		if (qName.equalsIgnoreCase("titulo")) {
-						bTitulo = true;
-					}
-            		
-					if (qName.equalsIgnoreCase("autor")) {
-						bAutor = true;
-					}
-
-					if (qName.equalsIgnoreCase("genero")) {
-						bGenero = true;
-					}
 					
-					if (qName.equalsIgnoreCase("descripcion")) {
-						bDescripcion = true;
-					}
 				}
 					
 				public void characters(char ch[], int start, int length) throws SAXException {
 					    
-					    if (bTitulo) {
-					    	System.out.println("Titulo: " + new String(ch, start, length));
-					    	bTitulo = false;
+					    if (bDepartamento) {
+					    	departamento = new String(ch, start, length);
+					    	System.out.println(id + " " + departamento);
+					    	bDepartamento = false;
 					    }
 					    
-					    if (bAutor) {
-					        System.out.println("Autor: " + new String(ch, start, length));
-					        bAutor = false;
-					    }
-					
-					    if (bGenero) {
-					    	System.out.println("Genero: " + new String(ch, start, length));
-					    	bGenero = false;
-				        }
-					    if (bDescripcion) {
-					    	System.out.println("Descripcion: " + new String(ch, start, length));
-					    	bDescripcion = false;
-				        }
 				    }
 					
 			};
 
-				File file = new File(path);
+				File file = new File("C:\\workspace_reto1\\DAM2Reto1Java\\ficheros\\departamentos.xml");
 				InputStream inputStream = new FileInputStream(file);
 				Reader reader = new InputStreamReader(inputStream, "UTF-8");
 				
