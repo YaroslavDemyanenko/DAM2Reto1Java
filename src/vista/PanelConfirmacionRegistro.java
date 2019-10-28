660bd374a292c37125049c15dad6666697de6c0d
IN1DM3B_02 <IN1DM3B_02@H015112.NET>
2019-10-28 13:30:01 +0100

    Pequeños cambios en Vistas

--- src/vista/PanelConfirmacionRegistro.java
+++ src/vista/PanelConfirmacionRegistro.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import javax.swing.JPanel;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class PanelConfirmacionRegistro extends JPanel {
 
@@ -13,6 +15,8 @@ public class PanelConfirmacionRegistro extends JPanel {
 	
 	private static final long serialVersionUID = -7175866429394634264L;
 
+	public JLabel lbl1, lbl2;
+	
 	/**
 	 * Create the panel.
 	 */
@@ -30,6 +34,15 @@ public class PanelConfirmacionRegistro extends JPanel {
 	private void instanciarObjetos() {
 		setLayout(null);
 		
+		lbl1 = new JLabel("\u00A1FELICIDADES!");
+		lbl1.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl1.setBounds(354, 249, 397, 35);
+		add(lbl1);
+		
+		lbl2 = new JLabel("Elemento generado correctamente");
+		lbl2.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl2.setBounds(354, 292, 397, 35);
+		add(lbl2);
+		
 	}
-
 }
