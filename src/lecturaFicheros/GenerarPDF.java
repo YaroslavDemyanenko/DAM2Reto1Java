5d74ac5e0210b7a58a3a1272a610405b57628504
IN1DM3B_22 <IN1DM3B_22@H015112.NET>
2019-11-12 09:50:51 +0100

    mas pdf

--- src/lecturaFicheros/GenerarPDF.java
+++ src/lecturaFicheros/GenerarPDF.java
@@ -4,6 +4,7 @@ import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
 
 import modelo.objetos.Departamento;
 import modelo.objetos.Empleado;
@@ -21,13 +22,20 @@ public class GenerarPDF {
 			}
 		}else if(departamentos != null) {
 			
-			//PdfPTable table = new PdfPTable(3);                
-            
-            //table.addCell("Celda 1");
+			Table tabla = new Table(3);
+			
+			tabla.addHeaderCell("ID");
+			tabla.addHeaderCell("Nombre dpto.");
+			tabla.addHeaderCell("Centro");
 			
 			for(int i=0; i < departamentos.length; i++) {
-				document.add(new Paragraph(departamentos[i].getId() + " " + departamentos[i].getNombre() + " " + departamentos[i].getCentro()));
+				//document.add(new Paragraph(departamentos[i].getId() + " " + departamentos[i].getNombre() + " " + departamentos[i].getCentro()));
+				
+				tabla.addCell(Integer.toString(departamentos[i].getId()));
+				tabla.addCell(departamentos[i].getNombre());
+				tabla.addCell(departamentos[i].getCentro().getNombre());
 			}
+			document.add(tabla);
 		}
 		
 		document.close();
